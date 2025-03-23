75fa5eb22cecd201b5a007f9cf7af4c28cb80138
Татаев Шокан Арсенұлы <shokan.tataev@bcchub.kz>
2025-03-23 16:10:39 +0500

    [chore]: setup project

---
+++ cmd/api/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+const (
+	envDev  = "dev"
+	envProd = "prod"
+)
+
+func main() {
+	fmt.Println("Starting API")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module social-network-api
+
+go 1.23.4
+
+require github.com/caarlos0/env/v11 v11.3.1 // indirect
